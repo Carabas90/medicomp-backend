72a8ec2abdadddf0e98ba75605b74a960d972c5d
Andreas Wagner <awagner90@outlook.de>
2022-05-08 20:17:29 +0200

    Implemented basic Repository using a JSON-File as Data-Storage

--- src/main/java/com/medicomp/medicompbackend/MedICompBackendApplication.java
+++ src/main/java/com/medicomp/medicompbackend/MedICompBackendApplication.java
@@ -9,5 +9,4 @@ public class MedICompBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedICompBackendApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/medicomp/medicompbackend/repository/MediRepository.java
@@ -0,0 +1,40 @@
+package com.medicomp.medicompbackend.repository;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.medicomp.medicompbackend.model.Medication;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MediRepository {
+    private List<Medication> meds;
+    private static final String path = "src/main/resources/MediRepository.json";
+
+    private MediRepository() {
+        this.meds = new ArrayList<>();
+    }
+
+    public static MediRepository loadRepository() throws FileNotFoundException {
+        Gson gson = new Gson();
+        JsonReader reader = gson.newJsonReader(new FileReader(path));
+        return gson.fromJson(reader, MediRepository.class);
+    }
+
+    public void saveRepository() throws IOException {
+        Writer writer = new FileWriter(path);
+        new Gson().toJson(this, writer);
+        writer.close();
+    }
+
+    public void addMed(Medication med) {
+        if (!meds.contains(med)) {
+            meds.add(med);
+        }
+    }
+
+    public List<Medication> getMeds() {
+        return meds;
+    }
+}
