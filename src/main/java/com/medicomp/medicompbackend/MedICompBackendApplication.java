48dc00e0bb77b11011623a0c5ab57e1fe26b1e04
Andreas Wagner <awagner90@outlook.de>
2022-05-08 19:25:16 +0200

    Initializing project

---
+++ src/main/java/com/medicomp/medicompbackend/MedICompBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medicomp.medicompbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedICompBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedICompBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicomp/medicompbackend/MedICompBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicomp.medicompbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedICompBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
